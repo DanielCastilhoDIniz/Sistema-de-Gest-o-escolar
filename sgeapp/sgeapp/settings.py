7f497ba4584ecb319a0eae521975ca4e59840ccb
Daniel Castilho <danielcastilhodiniz@gmail.com>
2024-12-30 01:20:35 -0300

    API documentation settings

--- sgeapp/sgeapp/settings.py
+++ sgeapp/sgeapp/settings.py
@@ -44,6 +44,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
+    'rest_framework',
+    'drf_spectacular',
+
 ]
 
 MIDDLEWARE = [
@@ -138,3 +141,8 @@ MEDIA_ROOT = DATA_DIR / 'media'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'core.User'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
--- sgeapp/sgeapp/urls.py
+++ sgeapp/sgeapp/urls.py
@@ -14,6 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+from drf_spectacular.views  import (
+    SpectacularAPIView,
+    SpectacularSwaggerView
+)
+
+
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
@@ -22,6 +29,12 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/schema/', SpectacularAPIView.as_view(), name='api-schema'),
+    path(
+        'api/docs/',
+        SpectacularSwaggerView.as_view(url_name='api-schema'),
+        name='api-docs',
+    ),
 ]
 
 # O código permite servir arquivos de mídia apenas no modo de desenvolvimento
